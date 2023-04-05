2f59eab61ee7c1387050357e15ae35cf620cfd43
Brice Piqueux <brice.piqueux@legrand-ext.com>
2023-04-05 16:46:45 +0200

    first usable version to get simple txt with test title + documentation

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from robot.testdoc import JsonConverter, TestSuiteFactory
-# import json
+import json
 
 datasources = ['CoAPs_user.robot']
 options = {'name': None, 'doc': None, 'metadata': [], 'settag': [], 'test': [], 'suite': [], 'include': [], 'exclude': []}
 suite = TestSuiteFactory(datasources)
 json_converter = JsonConverter()
 converted = json_converter.convert(suite)
-# with open("test2.json", "w") as output_file:
-#     output_file.write(json.dumps(converted))
-for test in converted['tests']:
-    print(test['name'])
-    print(test['doc'])
+with open("output.json", "w") as output_json:
+    output_json.write(json.dumps(converted))
+
+with open("output.txt", "w", encoding="utf_8") as output_txt:
+    for test in converted['tests']:
+        name = test['name'] + "\n"
+        doc = test['doc'].replace("<p>","").replace("</p>","") + "\n\n"
+        output_txt.write(name + doc)
